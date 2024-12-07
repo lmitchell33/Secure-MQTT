df632d581847eedf00352be42b832d710f7f8ff9
Lucas Mitchell <lhmitchell1333@gmail.com>
2024-12-07 18:34:11 -0500

    hopefully final commit

--- Publisher/mqtt_publisher.py
+++ Publisher/mqtt_publisher.py
@@ -2,12 +2,11 @@ import paho.mqtt.client as mqtt
 import os
 import ssl
 from time import sleep
-from datetime import datetime
 import logging
 
 
 class MQTTPublisher:
-    def __init__(self, broker:str, port:int, topic:str, username:str, password:str):
+    def __init__(self, broker, port, topic, username, password):
         '''Constructor for the MQTT subscriber class
         Args:
             broker {int} -- IP address for the broker/proxy to connect to
@@ -20,12 +19,23 @@ class MQTTPublisher:
             None
         '''
         
-        if broker is None or type(broker) != str:
+        if not isinstance(broker, str):
             raise TypeError("Broker argument is required and must be an string")
-        if port is None or type(port) != int:
+        if not isinstance(broker, str):
             raise TypeError("Port argument is required and must be an integer")
-        if topic is None or type(topic) != str:
+        if not isinstance(broker, str):
             raise TypeError("Topic argument is required and must be an integer")
+        if not isinstance(username, str):
+            raise TypeError("Password argument is required and must be an string")
+        if not isinstance(password, str):
+            raise TypeError("Username argument is required and must be an string")
+
+        # create and setup a logger 
+        self.logger = logging.getLogger("MQTTPublisher")
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler("mqtt_publisher.log")
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        self.logger.addHandler(handler)
         
         self.broker = broker
         self.port = port
@@ -35,27 +45,40 @@ class MQTTPublisher:
 
         base_dir = os.path.expanduser("~/SSL-IoT/Broker/config")
 
-        # create a client object (id=1) with the specified certificates
-        self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="publisher 2")
-        self.client.tls_set(
-            ca_certs=f'{base_dir}/certs/ca.crt',
-            certfile=f'certs/publisher.crt',
-            keyfile=f'certs/publisher.key',
-            cert_reqs=ssl.CERT_REQUIRED, 
-            tls_version=ssl.PROTOCOL_TLSv1_2
-        )
+        try:
+            # create a client object specifying the callback version and a unique id
+            self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="publisher_1")
+            self.client.tls_set(
+                ca_certs=f'{base_dir}/certs/ca.crt',
+                certfile=f'certs/publisher.crt',
+                keyfile=f'certs/publisher.key',
+                cert_reqs=ssl.CERT_REQUIRED, 
+                tls_version=ssl.PROTOCOL_TLSv1_2
+            )
+        except Exception as e:
+            self.logger.exception("Failed to setup TLS config")
+            raise e
 
         self.client.tls_insecure_set(False)
 
         # set the callback functions of the client object to the functions we created below
         self.client.on_connect = self.on_connect
-        # self.client.on_publish = self._on_publish
+        self.client.on_publish = self.on_publish
         
-        # TODO: uncomment this when authentication is setup
-        self.client.username_pw_set(username=self.username, password=self.password)
+        try:
+            # set the username and password combo
+            self.client.username_pw_set(username=self.username, password=self.password)
+        except Exception as e:
+            self.logger.exception("Failed to establish username and password with broker")
+            raise e
 
-        # NOTE: maybe want to change the keepalive to longer
-        self.client.connect(self.broker, self.port, 60)
+        try:
+            # connect to the broker
+            self.client.connect(self.broker, self.port, 60)
+            self.logger.info("Client successfully connected to broker")
+        except Exception as e:
+            self.logger.exception("Failed to connect to broker")
+            raise e
 
     
     def on_connect(self, client, userdata, flags, reason_code, properties):
@@ -72,25 +95,50 @@ class MQTTPublisher:
             None
         '''
 
-        if (reason_code == 0):
-            print(f"Successfully connected with result code {reason_code}")
+        if reason_code == 0:
+            self.logger.info(
+                f"Successfully connected to broker at {self.broker}:{self.port}. "
+                f"Client ID: {client._client_id.decode('utf-8')}, Topic: {self.topic}"
+            )
         else:
-            print(f"Connection failed with result code {reason_code}")
+            self.logger.error(
+                f"Connection to broker at {self.broker}:{self.port} failed. Reason Code: {reason_code}. "
+                f"Flags: {flags}. Potential issue with credentials or network."
+            )
 
     
-    def _on_publish(self, client, userdata, mid, reason_code, properties):
+    def on_publish(self, client, userdata, message_id, reason_code=None, properties=None):
         """Publish callback"""
-        print(f"Message {mid} pubslihed")
+        if reason_code == 0:
+            self.logger.info(
+                f"Successfully published to topic '{self.topic}' on broker {self.broker}:{self.port}. "
+                f"Message ID: {message_id}"
+            )        
+        else:
+            self.logger.error(
+                f"Failed to publish to topic '{self.topic}' on broker {self.broker}:{self.port}. ")
 
     
     def start(self):
-        self.client.loop_start()
+        try:
+            self.logger.info(
+                f"Starting MQTT publisher on broker {self.broker}:{self.port},"
+                f"Client ID: {self.client._client_id.decode('utf-8')}, Topic: {self.topic}"
+            )
+            self.client.loop_start()
+        except KeyboardInterrupt:
+            self.logger.info("MQTT publisher stopped by user")
+        except Exception as e:
+            self.logger.exception("{e} error occured while the subscriber was running.")
 
 
     def publish(self, message=None, qos=1, retain=False):
-        result = self.client.publish(self.topic, payload=message, qos=qos, retain=retain)
-        result.wait_for_publish()
-
+        try:
+            result = self.client.publish(self.topic, payload=message, qos=qos, retain=retain)
+            result.wait_for_publish()
+            self.logger.info(f"Publish request sent for topic '{self.topic}' to broker {self.broker}:{self.port}.")
+        except Exception as e:
+            self.logger.exception(f"Failed to send publish request for topic '{self.topic}'. Error: {e}")
 
 
 if __name__ == "__main__":
@@ -99,14 +147,18 @@ if __name__ == "__main__":
     public_IP = "24.3.166.47"
     username = "test_publisher"
     password = "mightyhippo917"
+    # username = "test"
+    # password = "test"
     internal_port = 443
     external_port = 333
 
 
-    publisher = MQTTPublisher(broker=public_IP, port=external_port, topic=topic, username=username, password=password)
+    # publisher = MQTTPublisher(broker=public_IP, port=external_port, topic=topic, username=username, password=password)
+    publisher = MQTTPublisher(broker=internal_IP, port=internal_port, topic=topic, username=username, password=password)
+
     publisher.start()
 
-    while True:
-        publisher.publish(message="Hello World")
+    while True:    
+        publisher.publish("Hello World")
         sleep(2)
     
--- Subscriber/mqtt_subscriber.py
+++ Subscriber/mqtt_subscriber.py
@@ -20,14 +20,25 @@ class MQTTSubscriber:
         Returns:
             None
         '''
-        
-        if broker is None or type(broker) != str:
+
+        if not isinstance(broker, str):
             raise TypeError("Broker argument is required and must be an string")
-        if port is None or type(port) != int:
+        if not isinstance(broker, str):
             raise TypeError("Port argument is required and must be an integer")
-        if topic is None or type(topic) != str:
+        if not isinstance(broker, str):
             raise TypeError("Topic argument is required and must be an integer")
-        
+        if not isinstance(username, str):
+            raise TypeError("Password argument is required and must be an string")
+        if not isinstance(password, str):
+            raise TypeError("Username argument is required and must be an string")
+
+        # create and setup a logger 
+        self.logger = logging.getLogger("MQTTSubscriber")
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler("mqtt_subscriber.log")
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        self.logger.addHandler(handler)
+
         self.topic = topic
         self.broker = broker
         self.port = port
@@ -36,27 +47,40 @@ class MQTTSubscriber:
 
         base_dir = os.path.expanduser("~/SSL-IoT/Broker/config")
 
-        # create a client object (id=1) with the specified certificates
-        self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="subscriber 1")
-        self.client.tls_set(
-            ca_certs=f'{base_dir}/certs/ca.crt',
-            certfile=f'certs/subscriber.crt',
-            keyfile=f'certs/subscriber.key',
-            cert_reqs=ssl.CERT_REQUIRED, 
-            tls_version=ssl.PROTOCOL_TLSv1_2
-        )
+        try:
+            # create a client object specifying the callback version and a unique id
+            self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="subscriber_1")
+            self.client.tls_set(
+                ca_certs=f'{base_dir}/certs/ca.crt',
+                certfile=f'certs/subscriber.crt',
+                keyfile=f'certs/subscriber.key',
+                cert_reqs=ssl.CERT_REQUIRED, 
+                tls_version=ssl.PROTOCOL_TLSv1_2
+            )
+        except Exception as e:
+            self.logger.exception("Failed to setup TLS config")
+            raise e
 
         self.client.tls_insecure_set(False)
 
         # set the callback functions of the client object to the functions we created below
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_disconnect = self.on_disconnect
         
-        # TODO: uncomment this when authentication is setup
-        self.client.username_pw_set(username=self.username, password=self.password)
+        try:
+            self.client.username_pw_set(username=self.username, password=self.password)
+        except Exception as e:
+            self.logger.exception("Failed to establish username and password with broker")
+            raise e
 
-        # NOTE: maybe want to change the keepalive to longer
-        self.client.connect(self.broker, self.port, 60)
+        try:
+            self.client.connect(self.broker, self.port, 60)
+            self.logger.info("Client successfully connected to broker")
+        except Exception as e:
+            self.logger.exception("Failed to connect to broker")
+            raise e
 
 
     def on_connect(self, client, userdata, flags, reason_code, properties):
@@ -73,40 +97,81 @@ class MQTTSubscriber:
             None
         '''
 
-        if (reason_code == 0):
-            print(f"Successfully connected with result code {reason_code}")
+        if reason_code == 0:
+            self.logger.info(
+                f"Successfully connected to broker at {self.broker}:{self.port}. "
+                f"Client ID: {client._client_id.decode('utf-8')}, Topic: {self.topic}"
+            )
         else:
-            print(f"Connection failed with result code {reason_code}")
+            self.logger.error(
+                f"Connection to broker at {self.broker}:{self.port} failed. Reason Code: {reason_code}. "
+                f"Flags: {flags}. Potential issue with credentials or network."
+            )
 
-        client.subscribe(self.topic)
-
+        self.subscribe(self.topic)
 
     def on_message(self, client, userdata, msg):
         ''' Message callback function to be called by the Client object to display the message received
         Args:
             client {obj} -- Paho-MQTT client object for the callback
-            userdata {obj} -- user data given to the callback function by the client
+            userdata {obj} -- user data given to the callback function by the client (typically none)
             msg {obj} -- msg given to the client obj about the topic
         Kwargs:
             None
         Returns:
             None
         '''
-        print(f"Received: {msg.topic} {msg.payload}")
+        message = msg.payload.decode('utf-8')
+
+        if len(message) > 2048: 
+            # becuase the broker is setup to not allow messages over a certain size, delete the message and log an error
+            self.logger.error(f"Size of message received from topic: '{self.topic}' is over the limit set by the broker. Size: {len(message)} bytes")
+            del message
+            return
+
+        self.logger.info(f"Message received on topic: '{msg.topic}' from broker {self.broker}:{self.port} and publisher. Size: {len(message)} bytes")
+
+        # NOTE: this print statement would typically either be a display for a UI or some sort of processing
+        print(f"Received: {message}")
+
+
+    def on_subscribe(self, client, usedata, message_id, granted_qos, properties=None):
+        if granted_qos[0] == 0:
+            self.logger.info(
+                f"Successfully subscribed to topic '{self.topic}' on broker {self.broker}:{self.port}. "
+                f"Granted QoS: {granted_qos}. Message ID: {message_id}"
+            )
+        else:
+            self.logger.error(
+                f"Failed to subscribe to topic '{self.topic}' on broker {self.broker}:{self.port}. ")
+
+
+    def on_disconnect(self, client, userdata, reason_code):
+        """Default on_disconnect callback."""
+        self.logger.info(f"Disconnected from broker with reason code {reason_code}")
+
+    def subscribe(self, topic):
+        try:
+            self.client.subscribe(topic)
+            self.logger.info(f"Subscription request sent for topic '{topic}' to broker {self.broker}:{self.port}.")
+        except Exception as e:
+            self.logger.exception(f"Failed to send subscription request for topic '{topic}'. Error: {e}")
 
 
     def start(self):
-        '''Function to start the Client object
-        Args: 
-            None
-        Kwargs:
-            None
-        Returns:
-            None
-        '''
+        '''Function to start the Client object'''
         # Because this program only wants to run one client, we can loop forever
         # otherwise, we would have to manually start and stop the loop
-        self.client.loop_forever()
+        try:
+            self.logger.info(
+                f"Starting MQTT subscriber on broker {self.broker}:{self.port},"
+                f"Client ID: {self.client._client_id.decode('utf-8')}, Topic: {self.topic}"
+            )
+            self.client.loop_forever()
+        except KeyboardInterrupt:
+            self.logger.info("MQTT subscriber stopped by user")
+        except Exception as e:
+            self.logger.exception("{e} error occured while the subscriber was running.")
 
 
 if __name__ == "__main__":
@@ -115,6 +180,8 @@ if __name__ == "__main__":
     public_IP = "24.3.166.47"
     username = "test_subscriber"
     password = "babyhippo917"
+    # bad_username = "awesomeelephant"
+    # bad_password = "I am a fish"
     internal_port = 443
     external_port = 333
 
